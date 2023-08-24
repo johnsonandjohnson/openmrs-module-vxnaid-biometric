04b99d184aed6b0612b6927f364d592df53c3ab6
Wargulak, Piotr [RNDBE NON-J&J] <PWargula@its.jnj.com>
2023-08-24 08:51:42 -0400

    3060: Added exceptions to make debugging wrong configuration easier

--- omod/src/main/java/org/openmrs/module/biometric/builder/VisitRequestBuilder.java
+++ omod/src/main/java/org/openmrs/module/biometric/builder/VisitRequestBuilder.java
@@ -4,17 +4,11 @@
  * OpenMRS is also distributed under the terms of the Healthcare Disclaimer located at
  * http://openmrs.org/license.
  *
- * <p>Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS graphic logo is
- * a trademark of OpenMRS Inc.
+ * <p>Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS graphic logo is a
+ * trademark of OpenMRS Inc.
  */
-
 package org.openmrs.module.biometric.builder;
 
-import java.text.ParseException;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import org.openmrs.Patient;
 import org.openmrs.Visit;
 import org.openmrs.VisitAttribute;
@@ -23,6 +17,7 @@ import org.openmrs.VisitType;
 import org.openmrs.api.PatientService;
 import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.biometric.api.exception.BiometricApiException;
 import org.openmrs.module.biometric.api.exception.EntityNotFoundException;
 import org.openmrs.module.biometric.contract.ParticipantAttribute;
 import org.openmrs.module.biometric.contract.VisitRequest;
@@ -33,25 +28,25 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-/**
- * Visit Request Builder.
- */
+import java.text.ParseException;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/** Visit Request Builder. */
 @Component
 public class VisitRequestBuilder {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(VisitRequestBuilder.class);
 
-  @Autowired
-  private BiometricModUtil util;
+  @Autowired private BiometricModUtil util;
 
-  @Autowired
-  private LocationUtil locationUtil;
+  @Autowired private LocationUtil locationUtil;
 
-  @Autowired
-  private VisitService visitService;
+  @Autowired private VisitService visitService;
 
-  @Autowired
-  private PatientService patientService;
+  @Autowired private PatientService patientService;
 
   /**
    * Creates visit entity from visit request.
@@ -59,14 +54,12 @@ public class VisitRequestBuilder {
    * @param request visit request
    * @return Visit @see org.openmrs.Visit
    */
-  public Visit createFrom(VisitRequest request) throws EntityNotFoundException, ParseException {
+  public Visit createFrom(VisitRequest request) throws BiometricApiException, ParseException {
     Visit newVisit = new Visit();
     newVisit.setUuid(request.getVisitUuid());
     Patient patient = patientService.getPatientByUuid(request.getParticipantUuid());
     newVisit.setPatient(patient);
-    //TO-DO check if visit type is null. This method is a fuzzy search. assuming there is one type with this visitType
-    VisitType visitType = visitService.getVisitTypes(request.getVisitType()).get(0);
-    newVisit.setVisitType(visitType);
+    newVisit.setVisitType(getVisitType(request.getVisitType()));
 
     Date startDatetime = util.convertIsoStringToDate(request.getStartDatetime());
     newVisit.setStartDatetime(startDatetime);
@@ -79,13 +72,13 @@ public class VisitRequestBuilder {
    * Creates visit entity from visit request and visit.
    *
    * @param request visit request
-   * @param visit   @see Visit
+   * @param visit @see Visit
    * @return Visit @see org.openmrs.Visit
    */
   public Visit createFrom(VisitRequest request, Visit visit) throws EntityNotFoundException {
     visit.setLocation(locationUtil.getLocationByUuid(request.getLocationUuid()));
 
-    //update visit attributes. if not exists then create otherwise update.
+    // update visit attributes. if not exists then create otherwise update.
     if (null != request.getAttributes() && !request.getAttributes().isEmpty()) {
       LOGGER.debug("No. of attributes : {}", request.getAttributes().size());
       Set<VisitAttribute> attributes = setVisitAttributes(request, visit);
@@ -102,8 +95,8 @@ public class VisitRequestBuilder {
     Set<VisitAttribute> attributes = new HashSet<>();
     for (ParticipantAttribute attribute : request.getAttributes()) {
       VisitAttribute visitAttribute = new VisitAttribute();
-      VisitAttributeType visitAttributeType = getVisitAttributeTypeByName(visitAttributeTypes,
-          attribute.getType());
+      VisitAttributeType visitAttributeType =
+          getVisitAttributeTypeByName(visitAttributeTypes, attribute.getType());
       if (null == visitAttributeType) {
         throw new EntityNotFoundException(
             String.format("Invalid visit attribute type : %s", attribute.getType()));
@@ -113,7 +106,7 @@ public class VisitRequestBuilder {
       visitAttribute.setValue(attribute.getValue());
       visitAttribute.setOwner(visit);
       visitAttribute.setCreator(Context.getAuthenticatedUser());
-      //for update request
+      // for update request
       if (null != request.getVisitUuid()) {
         visit.setAttribute(visitAttribute);
         visit.setDateChanged(new Date());
@@ -125,12 +118,11 @@ public class VisitRequestBuilder {
     return attributes;
   }
 
-
   /**
    * Fetches visit attribute type by name
    *
    * @param visitAttributeTypes list of visit attribute types
-   * @param name                name of the attribute type
+   * @param name name of the attribute type
    * @return VisitAttributeType @see org.openmrs.VisitAttributeType
    */
   private VisitAttributeType getVisitAttributeTypeByName(
@@ -143,4 +135,15 @@ public class VisitRequestBuilder {
     return null;
   }
 
+  private VisitType getVisitType(String visitTypeName) throws BiometricApiException {
+    List<VisitType> visitTypesForName = visitService.getVisitTypes(visitTypeName);
+
+    if (visitTypesForName.isEmpty()) {
+      throw new BiometricApiException("Missing Visit Type for name: " + visitTypeName);
+    } else if (visitTypesForName.size() > 1) {
+      throw new BiometricApiException("More then one Visit Type for name: " + visitTypeName);
+    }
+
+    return visitTypesForName.get(0);
+  }
 }
